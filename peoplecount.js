3a9d789c2d17a01b6296f8f856b506464394c9d3
takearide <102890984+takearide@users.noreply.github.com>
2022-04-25 17:43:54 +0530

    (Project - 2) Added New Functions Javascript

--- peoplecount.js
+++ peoplecount.js
@@ -1,15 +1,26 @@
+let total = 0
 let count = 0;
 function increment(){
     count += 1
     document.getElementById('count').textContent = count
 }
+function decrement() {
+    count -= 1
+    document.getElementById('count').textContent = count 
+}
 
 function save() {
     document.getElementById('save_el').textContent += count + "  - "
     document.getElementById('count').textContent = 0
+    sum()
     count = 0
-    console.log(count)
 }
-
+function sum() {
+    total += count
+    console.log(total)
+}
+function displaysum() {
+    document.getElementById('displaysum').textContent ="Total People Entered Today : " + total
+}
 
     
